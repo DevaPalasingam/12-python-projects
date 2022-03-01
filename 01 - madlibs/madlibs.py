18aa5699a0cc47c58b2c1de94626d129d5fda9b0
Palasingam <deva.palasingam@gmail.com>
2022-03-01 10:51:20 -0600

    first 4.5 projects completed

---
+++ 01 - madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+animal = input("Animal: ")
+
+madlib = f"Coding in Python is so {adj}! It makes me \
+{verb1} and {verb2} like a {animal}"
+
+print(madlib)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 12-python-projects
 One day this repository will be really cool
+
+Here's the link that I use:
+https://youtu.be/8ext9G7xspg
